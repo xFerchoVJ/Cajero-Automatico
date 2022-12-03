02315eec7e90a6656853a9ecd3eadded29df72e0
xFerchoVJ <fer_elsword@hotmail.com>
2022-12-02 21:07:45 -0600

    Create authenticate user system

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,19 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :lockable,
+         :recoverable, :rememberable, :validatable, password_length: 3..4
+
+  validates :card_digits, presence: true, length: {maximum: 16}
+  validates :password, presence: true, length: {minimum: 4, maximum: 4}
+  
+  #Use this method to change de auth way
+  def email_required?
+    false
+  end
+
+  # use this instead of email_changed? for rails >= 5.1
+  def will_save_change_to_email?
+    false
+  end
 end
